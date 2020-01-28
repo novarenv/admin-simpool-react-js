7311db240fb4c1caac460f220ca3e006d5e4987e
Novaren Veraldo <novarenv@gmail.com>
2020-01-28 17:56:11 +0700

    Add React OTP library

--- src/containers/Auth/Login/index.js
+++ src/containers/Auth/Login/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Input, CustomInput } from 'reactstrap';
 import Cookies from "js-cookie";
+import OtpInput from 'react-otp-input';
 
 import FormValidator from '../../../components/Forms/FormValidator.js';
 
 class Login extends Component {
 
   state = {
+    loginStatus: false,
+    OTPStatus: false,
+    OTP: '',
     formLogin: {
       username: 'novarenv',
       password: 'pensjoss',
@@ -15,11 +19,11 @@ class Login extends Component {
     }
   }
 
-   /**
-    * Validate input using onChange event
-    * @param  {String} formName The name of the form in the state object
-    * @return {Function} a function used for the event
-    */
+  /**
+   * Validate input using onChange event
+   * @param  {String} formName The name of the form in the state object
+   * @return {Function} a function used for the event
+   */
   validateOnChange = event => {
     const input = event.target;
     const form = input.form;
@@ -57,90 +61,130 @@ class Login extends Component {
     console.log(this.state.formLogin.password)
 
     e.preventDefault()
-    if(this.state.formLogin.username === this.state.formLogin.checkUsername && this.state.formLogin.password === this.state.formLogin.checkPassword ){
-      Cookies.set("loginToken", "Token", { expires: 7 })
-      this.props.history.push("/")
+    if (this.state.formLogin.username === this.state.formLogin.checkUsername && this.state.formLogin.password === this.state.formLogin.checkPassword) {
+      // Cookies.set("loginToken", "Token", { expires: 7 })
+      this.setState({
+        OTPStatus: true
+      })
+      console.log(this.state.OTPStatus)
+      // this.props.history.push("/")
     }
   }
 
   /* Simplify error check */
   hasError = (formName, inputName, method) => {
-    return  this.state[formName] &&
-        this.state[formName].errors &&
-        this.state[formName].errors[inputName] &&
-        this.state[formName].errors[inputName][method]
+    return this.state[formName] &&
+      this.state[formName].errors &&
+      this.state[formName].errors[inputName] &&
+      this.state[formName].errors[inputName][method]
+  }
+
+  changeOTP = () => {
+    console.log("OTP")
   }
 
   render() {
+    const LoginInput = () => {
+      return (
+        <div className="card-body">
+          <form className="mb-3" name="formLogin" onSubmit={this.onSubmit.bind(this)}>
+            <div className="form-group">
+              <label className="text-muted" htmlFor="username">Username</label>
+              <div className="input-group with-focus">
+                <Input
+                  type="text"
+                  name="username"
+                  className="border-right-0"
+                  placeholder="simpoool"
+                  invalid={this.hasError('formLogin', 'username', 'required')}
+                  onChange={this.validateOnChange}
+                  data-validate='["required"]'
+                  value={this.state.formLogin.username} />
+                <div className="input-group-append">
+                  <span className="input-group-text text-muted bg-transparent border-left-0">
+                    <em className="fa fa-envelope"></em>
+                  </span>
+                </div>
+                {this.hasError('formLogin', 'username', 'required') && <span className="invalid-feedback">Field is required</span>}
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="text-muted" htmlFor="resetInputEmail1">Password</label>
+              <div className="input-group with-focus">
+                <Input type="password"
+                  id="id-password"
+                  name="password"
+                  className="border-right-0"
+                  placeholder="Xxxx12345"
+                  invalid={this.hasError('formLogin', 'password', 'required')}
+                  onChange={this.validateOnChange}
+                  data-validate='["required"]'
+                  value={this.state.formLogin.password}
+                />
+                <div className="input-group-append">
+                  <span className="input-group-text text-muted bg-transparent border-left-0">
+                    <em className="fa fa-lock"></em>
+                  </span>
+                </div>
+                <span className="invalid-feedback">Field is required</span>
+              </div>
+            </div>
+            <div className="clearfix">
+              <CustomInput type="checkbox" id="remember-me"
+                className="float-left mt-0"
+                name="remember-me"
+                label="Remember Me">
+              </CustomInput>
+            </div>
+            <div className="text-center py-2">
+              <label className="c-radio">
+                <Input id="b-indonesia" type="radio" name="i-radio" defaultValue="b-indonesia" defaultChecked />
+                <span className="fa fa-circle"></span>Bahasa Indonesia</label>
+              <label className="c-radio">
+                <Input id="english" type="radio" name="i-radio" defaultValue="english" />
+                <span className="fa fa-circle"></span>English</label>
+            </div>
+            <button className="btn btn-block btn-primary mt-3 btn-color" type="submit">Login</button>
+          </form>
+        </div>
+      )
+    }
+
+    const OTP = () => {
+      return (
+        <div className="card-body">
+          <form className="mb-3" name="formOTP" onSubmit={this.onSubmit.bind(this)}>
+            <div>
+              <OtpInput 
+                inputStyle="otp"
+                numInputs={6}
+                onChange={otp => console.log(otp)}
+                separator="-"
+                value={this.state.OTP}
+              />
+
+              <Input 
+                type="tel" maxLength="1" className="otp"
+              />
+            </div>
+
+            <button className="btn btn-block btn-primary mt-3 btn-color" type="submit">Login</button>
+          </form>
+        </div>
+      )
+    }
+
     return (
       <div className="block-center mt-4 col-md-6">
         <div className="card card-flat">
           <div className="card-header text-center bg-dark">
             <div>
-              <img className="block-center rounded" src="img/Simpool Box.png" alt="Logo"/>
+              <img className="block-center rounded" src="img/Simpool Box.png" alt="Logo" />
             </div>
           </div>
-          <div className="card-body">
-            <form className="mb-3" name="formLogin" onSubmit={this.onSubmit.bind(this)}>
-              <div className="form-group">
-                <label className="text-muted" htmlFor="username">Username</label>
-                <div className="input-group with-focus">
-                  <Input
-                    type="text"
-                    name="username"
-                    className="border-right-0"
-                    placeholder="simpoool"
-                    invalid={this.hasError('formLogin','username','required')}
-                    onChange={this.validateOnChange}
-                    data-validate='["required"]'
-                    value={this.state.formLogin.username}/>
-                  <div className="input-group-append">
-                    <span className="input-group-text text-muted bg-transparent border-left-0">
-                      <em className="fa fa-envelope"></em>
-                    </span>
-                  </div>
-                  { this.hasError('formLogin','username','required') && <span className="invalid-feedback">Field is required</span> }
-                </div>
-              </div>
-              <div className="form-group">
-                <label className="text-muted" htmlFor="resetInputEmail1">Password</label>
-                <div className="input-group with-focus">
-                  <Input type="password"
-                    id="id-password"
-                    name="password"
-                    className="border-right-0"
-                    placeholder="Xxxx12345"
-                    invalid={this.hasError('formLogin','password','required')}
-                    onChange={this.validateOnChange}
-                    data-validate='["required"]'
-                    value={this.state.formLogin.password}
-                  />
-                  <div className="input-group-append">
-                    <span className="input-group-text text-muted bg-transparent border-left-0">
-                      <em className="fa fa-lock"></em>
-                    </span>
-                  </div>
-                  <span className="invalid-feedback">Field is required</span>
-                </div>
-              </div>
-              <div className="clearfix">
-                <CustomInput type="checkbox" id="remember-me"
-                  className="float-left mt-0"
-                  name="remember-me"
-                  label="Remember Me">
-                </CustomInput>
-              </div>
-              <div className="text-center py-2">
-                <label className="c-radio">
-                  <Input id="b-indonesia" type="radio" name="i-radio" defaultValue="b-indonesia" defaultChecked/>
-                  <span className="fa fa-circle"></span>Bahasa Indonesia</label>
-                <label className="c-radio">
-                  <Input id="english" type="radio" name="i-radio" defaultValue="english"/>
-                  <span className="fa fa-circle"></span>English</label>
-              </div>
-              <button className="btn btn-block btn-primary mt-3 btn-color" type="submit">Login</button>
-            </form>
-          </div>
+          {
+            this.state.OTPStatus ? (<OTP />) : (<LoginInput />)
+          }
         </div>
         <div className="p-3 text-center">
           <span className="mr-2">&copy;</span>
