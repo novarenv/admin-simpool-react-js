4cf0ed023af735146c6561a86237b7030294dc3f
Novaren Veraldo <novarenv@gmail.com>
2020-01-30 13:03:13 +0700

    Add Beginning Balance template

--- src/containers/Accounting/BeginningBalance/index.js
+++ src/containers/Accounting/BeginningBalance/index.js
@@ -6,28 +6,85 @@ import { Container, Card, CardBody } from 'reactstrap';
 
 import ContentWrapper from '../../../components/Layout/ContentWrapper';
 
-const grid = [
-  [{ value: 5, expr: '1 + 4' }, { value: 6, expr: '6' }, { value: new Date('2008-04-10') }],
-  [{ value: 5, expr: '1 + 4' }, { value: 5, expr: '1 + 4' }, { value: new Date('2004-05-28') }]
-]
-const onCellsChanged = (changes) => changes.forEach(({ cell, row, col, value }) => console.log("New expression :" + value))
-
 class NewAccountBalance extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      grid: [
+      rows: [
         [
-          { readOnly: true, value: '' },
-          { value: 'A', readOnly: true },
-          { value: 'B', readOnly: true },
-          { value: 'C', readOnly: true },
-          { value: 'D', readOnly: true }
+          {
+            value: "Beginning Balance",
+            forceComponent: true,
+            disableEvents: true,
+            colSpan: 11,
+            component: (
+              <div
+                className="d-flex justify-content-between px-3 py-3 align-items-center"
+                style={{ cursor: "default" }}
+              >
+                <div />
+                <h5 style={{ marginBottom: "0" }}>Beginning Balance</h5>
+                <div>
+                  <button
+                    className={`btn btn-sm btn-icon btn-primary mr-4`}
+                  >
+                    <i className="fas fa-minus" />
+                  </button>
+
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-icon btn-primary"
+                  >
+                    <i className="fas fa-plus" />
+                  </button>
+                </div>
+              </div>
+            )
+          }
         ],
-        [{ readOnly: true, value: 1 }, { value: 1 }, { value: 3 }, { value: 3 }, { value: 3 }],
-        [{ readOnly: true, value: 2 }, { value: 2 }, { value: 4 }, { value: 4 }, { value: 4 }],
-        [{ readOnly: true, value: 3 }, { value: 1 }, { value: 3 }, { value: 3 }, { value: 3 }],
-        [{ readOnly: true, value: 4 }, { value: 2 }, { value: 4 }, { value: 4 }, { value: 4 }]
+        [
+          {
+            readOnly: true,
+            value: `Account`,
+            rowSpan: 2,
+          },
+          { value: "Balance 1", readOnly: true, colSpan: 3 },
+          { value: "Balance 2", readOnly: true, colSpan: 3 },
+          { value: "Balance 3", readOnly: true, colSpan: 3 },
+          {
+            value: `Sum`,
+            rowSpan: 2,
+            readOnly: true,
+          }
+        ],
+        [
+          { value: "1.1", readOnly: true },
+          { value: "1.2", readOnly: true },
+          { value: "1.3", readOnly: true },
+          { value: "2.1", readOnly: true },
+          { value: "2.2", readOnly: true },
+          { value: "2.3", readOnly: true },
+          { value: "3.1", readOnly: true },
+          { value: "3.2", readOnly: true },
+          { value: "3.3", readOnly: true },
+        ],
+        [
+          {
+            readOnly: true,
+            value: `1 ~ 2`,
+            rowSpan: 1,
+          },
+          { value: "Data 1" },
+          { value: "Data 2" },
+          { value: "Data 3" },
+          { value: "Data 1" },
+          { value: "Data 2" },
+          { value: "Data 3" },
+          { value: "Data 1" },
+          { value: "Data 2" },
+          { value: "Data 3" },
+          { value: "Sum" },
+        ],
       ]
     }
   }
@@ -36,22 +93,23 @@ class NewAccountBalance extends Component {
     return (
       <ContentWrapper>
         <div className="content-heading">
-          <div>Data Pinjaman</div>
+          <div>Beginning Balance</div>
         </div>
         <Container fluid>
           <Card>
             <CardBody>
               <Datasheet
-                data={this.state.grid}
-                valueRenderer={(cell) => cell.value}
+                data={this.state.rows}
+                valueRenderer={cell => cell.value}
                 onContextMenu={(e, cell, i, j) => cell.readOnly ? e.preventDefault() : null}
                 onCellsChanged={changes => {
-                  const grid = this.state.grid.map(row => [...row])
+                  const rows = this.state.rows.map(row => [...row])
                   changes.forEach(({ cell, row, col, value }) => {
-                    grid[row][col] = { ...grid[row][col], value }
+                    rows[row][col] = { ...rows[row][col], value }
                   })
-                  this.setState({ grid })
+                  this.setState({ rows })
                 }}
+                className="col-12"
               />
             </CardBody>
           </Card>
