d98a0ee77bb4b9c9e1582fb5638595cd356644e7
Novaren Veraldo <novarenv@gmail.com>
2020-01-30 11:17:08 +0700

    Favicon and title Simpool

--- src/containers/Accounting/BeginningBalance/index.js
+++ src/containers/Accounting/BeginningBalance/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactDataSheet from 'react-datasheet';
+import Datasheet from 'react-datasheet';
 // Be sure to include styles at some point, probably during your bootstrapping
 import 'react-datasheet/lib/react-datasheet.css';
 import { Container, Card, CardBody } from 'reactstrap';
@@ -13,6 +13,25 @@ const grid = [
 const onCellsChanged = (changes) => changes.forEach(({ cell, row, col, value }) => console.log("New expression :" + value))
 
 class NewAccountBalance extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      grid: [
+        [
+          { readOnly: true, value: '' },
+          { value: 'A', readOnly: true },
+          { value: 'B', readOnly: true },
+          { value: 'C', readOnly: true },
+          { value: 'D', readOnly: true }
+        ],
+        [{ readOnly: true, value: 1 }, { value: 1 }, { value: 3 }, { value: 3 }, { value: 3 }],
+        [{ readOnly: true, value: 2 }, { value: 2 }, { value: 4 }, { value: 4 }, { value: 4 }],
+        [{ readOnly: true, value: 3 }, { value: 1 }, { value: 3 }, { value: 3 }, { value: 3 }],
+        [{ readOnly: true, value: 4 }, { value: 2 }, { value: 4 }, { value: 4 }, { value: 4 }]
+      ]
+    }
+  }
+
   render() {
     return (
       <ContentWrapper>
@@ -22,11 +41,17 @@ class NewAccountBalance extends Component {
         <Container fluid>
           <Card>
             <CardBody>
-              <ReactDataSheet
-                data={grid}
-                valueRenderer={(cell, i,  j) => j === 2 ? cell.value.toDateString() : cell.value}
-                dataRenderer={(cell, i, j) => j === 2 ? cell.value.toISOString() : cell.expr}
-                onCellsChanged={onCellsChanged}
+              <Datasheet
+                data={this.state.grid}
+                valueRenderer={(cell) => cell.value}
+                onContextMenu={(e, cell, i, j) => cell.readOnly ? e.preventDefault() : null}
+                onCellsChanged={changes => {
+                  const grid = this.state.grid.map(row => [...row])
+                  changes.forEach(({ cell, row, col, value }) => {
+                    grid[row][col] = { ...grid[row][col], value }
+                  })
+                  this.setState({ grid })
+                }}
               />
             </CardBody>
           </Card>
