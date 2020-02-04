61df7e0e5e633ba3612a7c482b2a4659532485a2
Novaren Veraldo <novarenv@gmail.com>
2020-02-04 09:38:11 +0700

    Add Login Translation

--- src/containers/Auth/Login/index.js
+++ src/containers/Auth/Login/index.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react';
 import { Input, CustomInput } from 'reactstrap';
 import Cookies from "js-cookie";
 import OtpInput from 'react-otp-input';
+import PropTypes from 'prop-types';
+import { withTranslation, Trans } from 'react-i18next';
 
 import FormValidator from '../../../components/Forms/FormValidator.js';
 
-class Login extends Component {
+import * as actions from '../../../store/actions/actions';
+import { connect } from 'react-redux';
+import { bindActionCreators, compose } from 'redux';
 
-  state = {
-    loginStatus: false,
-    OTPStatus: false,
-    OTP: '',
-    formLogin: {
-      username: 'novarenv',
-      password: 'pensjoss',
-      checkUsername: 'novarenv',
-      checkPassword: 'pensjoss'
+class Login extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      loginStatus: false,
+      OTPStatus: false,
+      OTP: '',
+      formLogin: {
+        username: 'novarenv',
+        password: 'pensjoss',
+        checkUsername: 'novarenv',
+        checkPassword: 'pensjoss'
+      }
     }
   }
 
@@ -86,19 +94,35 @@ class Login extends Component {
     console.log("OTP")
   }
 
+  changeLanguage = lng => {
+    this.props.actions.changeLanguage("language", lng)
+    if (lng === 'en') {
+      this.props.actions.changeDropdownLanguage("dropdownLanguage", "English")
+    } else if (lng === 'id') {
+      this.props.actions.changeDropdownLanguage("dropdownLanguage", "B. Indonesia")
+    }
+    this.props.i18n.changeLanguage(lng)
+  }
+
   render() {
+    let isId, isEn
+    if (this.props.dashboard.language === 'id')
+      isId = true
+    else if(this.props.dashboard.language === 'en')
+      isEn = true
+
     const LoginInput = () => {
       return (
         <div className="card-body">
           <form className="mb-3" name="formLogin" onSubmit={this.onSubmit.bind(this)}>
             <div className="form-group">
-              <label className="text-muted" htmlFor="username">Username</label>
+              <label className="text-muted" htmlFor="username"><Trans i18nKey='login.USERNAME'>Username</Trans></label>
               <div className="input-group with-focus">
-                <Input
+                <input
                   type="text"
                   name="username"
-                  className="border-right-0"
-                  placeholder="simpoool"
+                  className="form-control border-right-0 input-font-size"
+                  placeholder="contoh: simpool"
                   invalid={this.hasError('formLogin', 'username', 'required')}
                   onChange={this.validateOnChange}
                   data-validate='["required"]'
@@ -108,16 +132,16 @@ class Login extends Component {
                     <em className="fa fa-envelope"></em>
                   </span>
                 </div>
-                {this.hasError('formLogin', 'username', 'required') && <span className="invalid-feedback">Field is required</span>}
+                {this.hasError('formLogin', 'username', 'required') && <span className="invalid-feedback"><Trans i18nKey='forms.REQUIRED'>Form is required!</Trans></span>}
               </div>
             </div>
             <div className="form-group">
-              <label className="text-muted" htmlFor="resetInputEmail1">Password</label>
+              <label className="text-muted" htmlFor="resetInputEmail1"><Trans i18nKey='login.PASSWORD'>Password</Trans></label>
               <div className="input-group with-focus">
-                <Input type="password"
+                <input type="password"
                   id="id-password"
                   name="password"
-                  className="border-right-0"
+                  className="form-control border-right-0 input-font-size"
                   placeholder="Xxxx12345"
                   invalid={this.hasError('formLogin', 'password', 'required')}
                   onChange={this.validateOnChange}
@@ -129,25 +153,25 @@ class Login extends Component {
                     <em className="fa fa-lock"></em>
                   </span>
                 </div>
-                <span className="invalid-feedback">Field is required</span>
+                <span className="invalid-feedback"><Trans i18nKey='forms.REQUIRED'>Form is required!</Trans></span>
               </div>
             </div>
             <div className="clearfix">
               <CustomInput type="checkbox" id="remember-me"
                 className="float-left mt-0"
                 name="remember-me"
-                label="Remember Me">
+                label={<Trans i18nKey='login.REMEMBER_ME'>Remember Me</Trans>}>
               </CustomInput>
             </div>
             <div className="text-center py-2">
               <label className="c-radio">
-                <Input id="b-indonesia" type="radio" name="i-radio" defaultValue="b-indonesia" defaultChecked />
+                <Input id="id" type="radio" name="i-radio" defaultValue="id" defaultChecked={isId} onClick={() => this.changeLanguage('id')}/>
                 <span className="fa fa-circle"></span>Bahasa Indonesia</label>
               <label className="c-radio">
-                <Input id="english" type="radio" name="i-radio" defaultValue="english" />
+                <Input id="en" type="radio" name="i-radio" defaultValue="en" defaultChecked={isEn} onClick={() => this.changeLanguage('en')} />
                 <span className="fa fa-circle"></span>English</label>
             </div>
-            <button className="btn btn-block btn-primary mt-3 btn-color" type="submit">Login</button>
+            <button className="btn btn-block btn-primary mt-3 btn-color" type="submit"><Trans i18nKey='login.LOGIN'>Login</Trans></button>
           </form>
         </div>
       )
@@ -200,4 +224,12 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  actions: PropTypes.object,
+  dashboard: PropTypes.object
+}
+
+const mapStateToProps = state => ({ dashboard: state.dashboard })
+const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(actions, dispatch) })
+
+export default compose(connect(mapStateToProps, mapDispatchToProps), withTranslation('translations'))(Login);
